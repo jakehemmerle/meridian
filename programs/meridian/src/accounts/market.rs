0988e7fc358cd7f3bb3a27b1d81a57f4354b7141
jakehemmerle <jakehemmerle@protonmail.com>
2026-03-11 17:27:29 -0500

    br-1aq: finish oracle and account helpers split

--- programs/meridian/src/accounts/market.rs
+++ programs/meridian/src/accounts/market.rs
@@ -2,4 +2,10 @@ use anchor_lang::prelude::*;
 
 use crate::MeridianMarket;
 
+use super::TokenAccountRefs;
+
 pub type MeridianMarketAccount<'info> = Account<'info, MeridianMarket>;
+
+pub fn market_token_accounts(market: &MeridianMarket) -> TokenAccountRefs {
+    TokenAccountRefs::from_market(market)
+}
--- programs/meridian/src/accounts/token_accounts.rs
+++ programs/meridian/src/accounts/token_accounts.rs
@@ -1,8 +1,24 @@
 use anchor_lang::solana_program::pubkey::Pubkey;
 
+use crate::MeridianMarket;
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct TokenAccountRefs {
     pub yes_mint: Pubkey,
     pub no_mint: Pubkey,
     pub vault: Pubkey,
+}
+
+impl TokenAccountRefs {
+    pub const fn new(yes_mint: Pubkey, no_mint: Pubkey, vault: Pubkey) -> Self {
+        Self {
+            yes_mint,
+            no_mint,
+            vault,
+        }
+    }
+
+    pub fn from_market(market: &MeridianMarket) -> Self {
+        Self::new(market.yes_mint, market.no_mint, market.vault)
+    }
 }
